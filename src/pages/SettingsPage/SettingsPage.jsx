008a36bf2e07bd8bc2b866af5f79c9ab9f85e497
Mohamed <mfarah37@gmail.com>
2022-11-30 22:50:04 -0800

    test commit

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -4,7 +4,7 @@ import ThemeSwitcher from "../../components/ThemeSwitcher/ThemeSwitcher";
 export default function SettingsPage() {
     return(
         <div>
-            <h1>Settings</h1>
+            <h1>settings</h1>
             <ThemeSwitcher />
         </div>
     )
